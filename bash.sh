bce72f770dffa019e53b45c0b7add3ddc05017f3
QJ刘贵生 <codeliuguisheng@163.com>
2020-12-03 16:06:23 +0800

    first commit

--- bash.sh
+++ bash.sh
@@ -8,9 +8,18 @@ function obtain_git_branch {
 }
 branch=`obtain_git_branch`
 
+git add .
 
+git commit -m "first commit"
 
-echo dev代码合并完毕，正在打包组件库...
+echo 当前版本号: $branch
+
+git push --set-upstream origin $branch
+
+sleep 1s
+
+echo 代码已推送到远程分支，正在打包组件库...
+
 
 npm run org
 
@@ -22,16 +31,6 @@ echo 组件库打包完毕,正在发布,请稍等
 
 sleep 1s
 
-npm run publish
+npm publish
 
-git add .
 
-git commit -m "first commit"
-
-echo 当前版本号: $branch
-
-git push --set-upstream origin $branch
-
-sleep 1s
-
-echo 代码已推送到远程分支
